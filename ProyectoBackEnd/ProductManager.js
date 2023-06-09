e3e276361ebb055a2c5f45e11b95ffcd38fad8a9
santiago Pena <psanti89@gmail.com>
2023-06-09 16:03:30 -0300

    test

--- ProyectoBackEnd/ProductManager.js
+++ ProyectoBackEnd/ProductManager.js
@@ -6,6 +6,7 @@ class ProductManager {
     #id = 0;
     constructor(path){
         this.products = [];
+        asdasd
         this.path= path;
     }
     // Creates product if fields are not null and if the code is unique. Pushes it to products array
